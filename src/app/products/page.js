0c4243bc7777fac081d6f0bfbd328e51fa86619d
Jawairia <Jawairiamazhar21@gmail.com>
2026-07-26 23:12:35 +0500

    feat: add search functionality to products page

--- src/app/products/page.js
+++ src/app/products/page.js
@@ -1,13 +1,23 @@
 import { getProducts } from "@/lib/api";
 import ProductGrid from "@/components/ProductGrid";
+import SearchBar from "@/components/SearchBar"
 
-export default async function ProductsPage() {
+export default async function ProductsPage({searchParams}) {
   const products = await getProducts();
+  const { q: query } = await searchParams; //pulls out the q property specifically and stores it in a variable named query 
+  const filteredProducts = () => {
+    if (query == null || query === undefined || query === "") {
+      return products;}
+    else{
+      return products.filter(product => product.title.toLowerCase().includes(query.toLowerCase()))
+    }
+  }
 
   return (
     <main className="max-w-7xl mx-auto px-4 py-8">
+      <SearchBar />
       <h1 className="text-2xl font-bold text-gray-900 mb-6">All Products</h1>
-      <ProductGrid products={products} />
+      <ProductGrid products={filteredProducts()} /> 
     </main>
   );
 }
---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+"use client"
+
+import React from 'react'
+import { useRouter } from "next/navigation"
+
+const SearchBar = () => {
+    const [searchText, setSearchText] = React.useState("");
+    const router = useRouter();
+    const handleSearch = (e) => {
+        e.preventDefault();
+        router.push(`/products?q=${searchText}`);
+    }
+
+  return (
+    <div className = "flex items-center justify-center mb-4">
+        <input 
+        value={searchText} 
+        onChange={(e)=> {setSearchText(e.target.value)}}
+        className="border border-2-pink"/>
+        <button onClick={handleSearch} className="border border-2-black">Search</button>
+    </div>
+  )
+}
+
+export default SearchBar
